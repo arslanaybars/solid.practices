5a574787e714ff796720ed29bd42b5b4618561b5
Aybars Arslan <arslanaybars@gmail.com>
2019-05-16 23:13:11 +0300

    Project init

---
+++ solid.practices/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace solid.practices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ardalis Insurance Rating System Starting...");
+
+            var engine = new RatingEngine();
+            engine.Rate();
+
+            if (engine.Rating > 0)
+            {
+                Console.WriteLine($"Rating: {engine.Rating}");
+            }
+            else
+            {
+                Console.WriteLine("No rating produced.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
